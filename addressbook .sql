4ac42089663dbc85fc30b3b0fe6b296233e5fb71
KratiGupta <kratigupta8545@gmail.com>
2021-08-25 19:49:41 +0530

    Added to edit contacts

--- addressbook .sql
+++ addressbook .sql
@@ -17,6 +17,7 @@ Create table address_book(
 INSERT INTO address_book (firstname,lastname,address,city,state,zip,phone_number,email_address) VALUES
    ('Krati','Gupta','kidwai nagar','Kanpur','UP','208011','9832779377','k@gmail.com'),
     ('Nishu','Tyagi','gzb','Ghaziabad','UP','276388','8254567899','N@gmail.com');
-
-
 SELECT * FROM address_book;
+
+UPDATE address_book set email_address = 'kratigupta8545@gmail.com' WHERE firstname = 'Krati';
+ Select * from address_book;
