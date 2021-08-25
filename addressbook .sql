2f9c7dbf765586a3fbdd1fc61a03f338bf380c7e
KratiGupta <kratigupta8545@gmail.com>
2021-08-25 19:44:20 +0530

    Added creation of address book table

--- addressbook .sql
+++ addressbook .sql
@@ -2,3 +2,14 @@ CREATE DATABASE addressbook;
 show databases;
  USE addressbook;
 
+
+Create table address_book(
+     firstname    VARCHAR(150) NOT NULL,
+     lastname    VARCHAR(150) NOT NULL,
+     address     VARCHAR(150) NOT NULL,
+     city      VARCHAR(150) NOT NULL,
+     state   VARCHAR(150) NOT NULL,
+     zip     INT(6) NOT NULL,
+     phone_number  VARCHAR(150) NOT NULL,
+     email_address  VARCHAR(150) NOT NULL
+     );
